e6bdf8daaba6a8d31cf185884f26777ed00d679e
Jake Moss <jake.moss@uqconnect.edu.au>
2022-02-02 19:31:06 +1000

    Init

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject course-bot "0.1.0-SNAPSHOT"
+  :description "Discord bot to handle UQ courses"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.suskalo/discljord "1.1.1"]]
+  :repl-options {:init-ns course-bot.core}
+  :main course-bot.core)
