c6807fe4bc41fd27498b363f0cda6f93da24b034
Jake Moss <jake.moss@uqconnect.edu.au>
2022-07-26 15:45:18 +1000

    Tweaks

--- src/course_bot/scrapper.clj
+++ src/course_bot/scrapper.clj
@@ -1,23 +1,31 @@
 (ns course-bot.scrapper
-  (:require [net.cgrand.enlive-html :refer :all])
+  (:require [net.cgrand.enlive-html :as html])
   (:import java.net.URL))
 
+(def base-url "https://my.uq.edu.au/programs-courses/course.html?course_code=")
 
-(defn yoink-details [course-code]
-  "Scrap the details of all availiable course offersings from UQ"
-  (let [html (html-resource (URL. (str "https://my.uq.edu.au/programs-courses/course.html?course_code=" course-code)))]
-    {:desc (first (select html [:#course-summary content]))
+(defn get-html
+  "A memorised function to retreive html."
+  [url]
+   (html/html-resource (URL. url)))
+
+(defn yoink-details
+  "Scrap the details of all availiable course offerings from UQ"
+  [course-code]
+  (let [html (get-html (str base-url course-code))]
+    {:name (first (html/select html [:#course-title html/content]))
+     :desc (first (html/select html [:#course-summary html/content]))
      :offerings
-     (for [current-offerings (select html [:table#course-current-offerings :tbody :tr])]
-       {:year (first (select current-offerings
-                             [:a.course-offering-year content]))
+     (for [current-offerings (html/select html [:table#course-current-offerings :tbody :tr])]
+       {:sem (first (html/select current-offerings
+                                 [:a.course-offering-year html/content]))
 
-        :mode (first (select current-offerings
-                             [:td.course-offering-mode :a content]))
+        :mode (first (html/select current-offerings
+                                  [:td.course-offering-mode :a html/content]))
 
-        :location (first (select current-offerings
-                                 [:td.course-offering-location content]))
+        :location (first (html/select current-offerings
+                                      [:td.course-offering-location html/content]))
 
         :profile (:href (:attrs
-                         (first (select current-offerings
-                                        [:td.course-offering-profile :a]))))})}))
+                         (first (html/select current-offerings
+                                             [:td.course-offering-profile :a]))))})}))
