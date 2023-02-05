aa4ea7e74e59773e56a1eeca44100447d142a064
Jake Moss <jake.moss@uqconnect.edu.au>
2023-02-05 13:50:47 +1000

    Enabled proper logging and use my own fork

--- project.clj
+++ project.clj
@@ -2,11 +2,16 @@
   :description "Discord bot to handle UQ courses"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.github.discljord/discljord "1.3.1"]
+  :repositories [["jitpack" "https://jitpack.io"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/tools.logging "1.2.4"]
+                 [ch.qos.logback/logback-classic "1.4.5"]
+                 ;; [com.github.discljord/discljord "1.3.1"]
+                 [com.github.Jake-Moss/discljord "4067dcc0df"]
                  [enlive "1.1.6"]
                  [com.github.johnnyjayjay/slash "0.5.0-SNAPSHOT"]
                  [cljplot "0.0.2a-SNAPSHOT"]]
   :repl-options {:init-ns course-bot.core}
   :plugins [[lein-cljfmt "0.8.0"]]
+  :jvm-opts ["-Dclojure.tools.logging.factory=clojure.tools.logging.impl/slf4j-factory"]
   :main course-bot.core)
